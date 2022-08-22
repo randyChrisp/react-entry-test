0e1fd0476ab5bd6cefd914fce42448dc91daf346
randyChrisp <randy.chrisp@worldgrouplogistics.com>
2022-08-22 13:13:45 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
+import React from 'react';
 import logo from './logo.svg';
+import { Button } from '@mui/material';
 import './App.css';
 
 function App() {
+  let [numberHeader, setNumberHeader] = React.useState({num: 0, log: "Started Log at 0"});
+
+  let randomNumber = 0;
+  let incrementNumber = 5;
+
+  <h1>Number Header Is {numberHeader}</h1>
+
+  const onRandomButtonClick = () => {
+    randomNumber = { 
+    num: Math.floor(Math.random() * 100),
+    log: `Random number to ${numberHeader.num}`    
+    }
+
+    setNumberHeader(randomNumber);
+    console.log(randomNumber);
+    console.log(numberHeader);
+  }
+  
+  const onIncrementButtonClick = () => {
+    incrementNumber = {
+      num: numberHeader.num + incrementNumber,
+      log: `Incremented number to ${numberHeader.num}`
+    }
+  
+    if(incrementNumber > 100)
+      alert("The maximum number is 100.");
+  
+    setNumberHeader(incrementNumber);
+    console.log(numberHeader.num)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React</h1>
+
+      <Button id='randomButton' variant='outlined' onClick={onRandomButtonClick}>Random Number {numberHeader.num}</Button>&nbsp;
+      <Button id='incrementButton' variant='outlined' onClick={onIncrementButtonClick}>Increment Number {incrementNumber}</Button>
+
+      <br></br>
+      <br></br>
+      
+      <textarea rows={10} cols={10}>
+        <p>{randomNumber.log}</p>
+        <p>{incrementNumber.log}</p>
+      </textarea>
+      
+      <div>
+        <p> By: Randy Chrisp - 8/20//2022</p>
+        <p>	&#169; 2019 World Shipping, Inc.</p>
+      </div>
     </div>
   );
 }
